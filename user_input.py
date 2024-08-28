f3b365ce3f0ba5f0d13fb166cc459ac002b71a54
Nderitu-Dev <bonifacemaina.w@gmail.com>
2024-08-28 11:31:42 +0300

    Python assignment 1

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input
+
+# Name Prompt
+name = input("What is your name?");
+
+# Age Prompt
+age = input("How old are you?");
+
+# Location Prompt
+city = input("Which city do you come from?");
+
+# Personalized message
+print(f"Hello {name}, you are {28} years old and you are from {city}.")
